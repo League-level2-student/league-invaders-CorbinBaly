f9f1d3a833e736ec1634e2ca069bdbff63310232
league <league@imac-18.attlocal.net>
2019-08-03 11:26:00 -0700

    Worked on LeagueInvaders to part5

---
+++ src/GameObject.java
@@ -0,0 +1,23 @@
+import java.awt.Graphics;
+
+public class GameObject {
+	int x;
+	int y;
+	int width;
+	int height;
+
+	public GameObject(int x, int y, int width, int height) {
+		x = this.x;
+		y = this.y;
+		width = this.width;
+		height = this.height;
+	}
+
+	void update() {
+
+	}
+
+	void draw(Graphics g) {
+		g.fillRect(10, 10, 100, 100);
+	}
+}
--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,30 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+	final int width = 500;
+	final int height = 800;
+	JFrame frame;
+	GamePanel gamepanel;
 
+	public LeagueInvaders() {
+		frame = new JFrame();
+		gamepanel = new GamePanel();
+	}
+
+	void setup() {
+		frame.add(gamepanel);
+		frame.addKeyListener(gamepanel);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(3);
+		frame.getContentPane().setPreferredSize(new Dimension(width, height));
+		frame.pack();
+		gamepanel.startGame();
+	}
+
+	public static void main(String[] args) {
+		LeagueInvaders invader = new LeagueInvaders();
+		invader.setup();
+	}
 }
